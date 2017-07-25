ebf09079470e596074546bc5b7e9e2800a8a60e5
Micheál Looney <hello@micheallooney.com>
2017-07-25 13:03:25 +0100

    Moar hacks

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -5,6 +5,7 @@ class CompaniesController < ApplicationController
   def index
     if params.has_key?(:intercom_id)
       @company = IntercomUtil::get_company(params[:intercom_id])
+      @company_convo_count = Conversation.where(company_id: params[:intercom_id]).count
     end    
   end
 end
--- app/helpers/company_helper.rb
+++ app/helpers/company_helper.rb
@@ -1,2 +1,24 @@
 module CompanyHelper
+
+    def get_support_cost
+      return @company_convo_count * 20
+    end  
+
+
+    def get_cost_status
+      
+      total_support_cost = @company_convo_count * 20
+      total_spend = @company.custom_attributes[:total_spend]
+
+
+      if total_spend > total_support_cost
+      return "🙂"
+      elsif total_spend == total_support_cost
+      return "😐"
+      else
+      return "😢"
+      end
+        
+    end
+
 end
